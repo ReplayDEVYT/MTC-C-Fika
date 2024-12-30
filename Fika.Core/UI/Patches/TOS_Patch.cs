ae3a0383683164be1fb559edbec4e4260b65c406
MissinginAction <91987199+ReplayDEVYT@users.noreply.github.com>
2024-12-30 19:28:11 +0000

    sync with main branch changes (minus the config changes)

--- Fika.Core/UI/Patches/TOS_Patch.cs
+++ Fika.Core/UI/Patches/TOS_Patch.cs
@@ -12,8 +12,8 @@ namespace Fika.Core.UI.Patches
 {
 	public class TOS_Patch : ModulePatch
 	{
-		protected const string str_1 = "V2VsY29tZSB0byBGaWthIQoKRmlrYSBpcyBhIGNvLW9wIG1vZCBmb3IgU1BULCBhbGxvd2luZyB5b3UgdG8gcGxheSB3aXRoIHlvdXIgZnJpZW5kcy4gRmlrYSBpcyBhbmQgd2lsbCBhbHdheXMgYmUgZnJlZSwgaWYgeW91IHBhaWQgZm9yIGl0IHlvdSBnb3Qgc2NhbW1lZC4gWW91IGFyZSBhbHNvIG5vdCBhbGxvd2VkIHRvIGhvc3QgcHVibGljIHNlcnZlcnMgd2l0aCBtb25ldGl6YXRpb24gb3IgZG9uYXRpb25zLgoKV2FpdCBmb3IgdGhpcyBtZXNzYWdlIHRvIGZhZGUgdG8gYWNjZXB0IG91ciBUZXJtcyBvZiBTZXJ2aWNlLgoKWW91IGNhbiBqb2luIG91ciBEaXNjb3JkIGhlcmU6IGh0dHBzOi8vZGlzY29yZC5nZy9wcm9qZWN0LWZpa2E=";
-		protected const string str_2 = "V2VsY29tZSB0byBGaWthIQoKRmlrYSBpcyBhIGNvLW9wIG1vZCBmb3IgU1BULCBhbGxvd2luZyB5b3UgdG8gcGxheSB3aXRoIHlvdXIgZnJpZW5kcy4gRmlrYSBpcyBhbmQgd2lsbCBhbHdheXMgYmUgZnJlZSwgaWYgeW91IHBhaWQgZm9yIGl0IHlvdSBnb3Qgc2NhbW1lZC4gWW91IGFyZSBhbHNvIG5vdCBhbGxvd2VkIHRvIGhvc3QgcHVibGljIHNlcnZlcnMgd2l0aCBtb25ldGl6YXRpb24gb3IgZG9uYXRpb25zLgoKWW91IGNhbiBqb2luIG91ciBEaXNjb3JkIGhlcmU6IGh0dHBzOi8vZGlzY29yZC5nZy9wcm9qZWN0LWZpa2E=";
+		protected const string str_1 = "V2VsY29tZSB0byBNVEMtQyEKCk1UQy1DIHVzZXMgRmlrYSBhcyBhIGJhc2UgZm9yIG91ciBtdWx0aXBsYXllci4gRmlrYSBpcyBhIGNvLW9wIG1vZCBmb3IgU1BUIHRoYXQgbGV0cyB5b3UgcGxheSBjb29wIEVGVCB3aXRoIHlvdXIgZnJpZW5kcy4gRmlrYSBpcyBmcmVlLCBpZiB5b3UgcGFpZCB0byBqb2luIHRoaXMgc2VydmVyIG9yIGFueSBvdGhlciBzZXJ2ZXIsIHlvdSBnb3Qgc2NhbW1lZC4gWW91IGFyZSBub3QgYWxsb3dlZCB0byBob3N0IHB1YmxpYyBzZXJ2ZXJzIHdpdGggbW9uZXRpemF0aW9uIG9yIGRvbmF0aW9ucy4gQW55IHNlcnZlcnMgdGhhdCBoYXZlIHBheW1lbnQgb2YgYW55IGtpbmQgd2lsbCBiZSBzaHV0IGRvd24uCgpXYWl0IGZvciB0aGlzIG1lc3NhZ2UgdG8gZmFkZSB0byBhY2NlcHQgdGhlIEZpa2EgVGVybXMgb2YgU2VydmljZS4KCllvdSBjYW4gam9pbiB0aGUgRmlrYSBEaXNjb3JkIGhlcmU6IGh0dHBzOi8vZGlzY29yZC5nZy9wcm9qZWN0LWZpa2EKClRoYW5rIHlvdSB0byB0aGUgRmlrYSBkZXYgdGVhbSBmb3IgbWFraW5nIHN1Y2ggYSB3b25kZXJmdWwgcGx1Z2luIDwz";
+		protected const string str_2 = "V2VsY29tZSB0byBNVEMtQyEKCk1UQy1DIHVzZXMgRmlrYSBhcyBhIGJhc2UgZm9yIG91ciBtdWx0aXBsYXllci4gWW91IGFyZSBub3QgYWxsb3dlZCB0byBob3N0IHB1YmxpYyBzZXJ2ZXJzIHdpdGggbW9uZXRpemF0aW9uIG9yIGRvbmF0aW9ucy4gQW55IHNlcnZlcnMgdGhhdCBoYXZlIHBheW1lbnQgb2YgYW55IGtpbmQgd2lsbCBiZSBzaHV0IGRvd24uCgpZb3UgY2FuIGpvaW4gdGhlIEZpa2EgRGlzY29yZCBoZXJlOiBodHRwczovL2Rpc2NvcmQuZ2cvcHJvamVjdC1maWthCgpUaGFuayB5b3UgdG8gdGhlIEZpa2EgZGV2IHRlYW0gZm9yIG1ha2luZyBzdWNoIGEgd29uZGVyZnVsIHBsdWdpbiA8Mw==";
 
 		private static bool HasShown = false;
 
@@ -47,14 +47,14 @@ namespace Fika.Core.UI.Patches
 			{
 				byte[] str_1_b = Convert.FromBase64String(str_1);
 				string str_1_d = Encoding.UTF8.GetString(str_1_b);
-				Singleton<PreloaderUI>.Instance.ShowFikaMessage("FIKA", str_1_d, ErrorScreen.EButtonType.QuitButton, 30f,
+				Singleton<PreloaderUI>.Instance.ShowFikaMessage("FIKA AND MTC-C", str_1_d, ErrorScreen.EButtonType.QuitButton, 15f,
 					Application.Quit, AcceptTos);
 			}
 			else
 			{
 				byte[] str_2_b = Convert.FromBase64String(str_2);
 				string str_2_d = Encoding.UTF8.GetString(str_2_b);
-				Singleton<PreloaderUI>.Instance.ShowFikaMessage("FIKA", str_2_d, ErrorScreen.EButtonType.OkButton, 0f,
+				Singleton<PreloaderUI>.Instance.ShowFikaMessage("FIKA AND MTC-C", str_2_d, ErrorScreen.EButtonType.OkButton, 0f,
 					null,
 					null);
 			}
